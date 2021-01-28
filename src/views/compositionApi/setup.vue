8f355b305fdb74cff23f1fbcf3bcd0f65a0ee1e1
niu <2659546337@qq.com>
2021-01-28 20:33:58 +0800

    提交

---
+++ src/views/compositionApi/setup.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>setup</h1>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "setup",
+  setup () {
+    console.log('---------setup------------');
+  },
+  beforeCreate () {
+    console.log('---------beforeCreate------------');
+  },
+  created () {
+    console.log('------------created------------');
+  },
+  beforeMount () {
+    console.log('------------beforeMount------------');
+  },
+  mounted () {
+    console.log('------------mounted------------');
+  },
+});
+</script>
---
+++ src/views/reactivityApi/reactive.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ user.name }}+{{ user.age }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, reactive } from 'vue';
+
+export default defineComponent({
+  name: "reactive",
+  setup () {
+    const user = reactive<{ name: string; age: number }>({
+      name: 'nxy',
+      age: 24
+    });
+    onMounted(() => {
+      setTimeout(() => {
+        user.age += 1;
+      }, 2000);
+    });
+    return {
+      user
+    };
+  }
+});
+</script>
+
+<style scoped>
+</style>
